48d41cf9b0b6a523f6fd65676b5bcd7ba8682c02
RintFosk <27263945+RintFosk@users.noreply.github.com>
2019-12-11 02:00:14 +0800

    adding the comment

--- etc/script/functions.js
+++ etc/script/functions.js
@@ -1,20 +1,32 @@
 function getRNG(max) {
+    /**
+     * Generate a random integer from 0 to (max-1)
+     */
     return Math.floor(Math.random() * (max));
 }
 
 function randomDeckGen(json, is_side_restricted = 0){
+    /**
+     * Generate a random deck combination, faction side restriction is false by default
+     */
+
     let deck = '';
-    let res;
+    // Need to define what side the deck will be 
+    let side;
+
+    // if the side restriction is false, randomly decide the deck's faction within full range
     if (is_side_restricted == 0){
-        res = getRNG(2);
+        side = getRNG(2);
     }
+    // if the side restriction is on, force the side to be the parameter defined, 1 = blue, 2 = red
     else {
-        res = is_side_restricted;
+        side = is_side_restricted - 1;
     }
         
-    console.log(res)
+    console.log(side)
 
-    if (res == 1) {
+    // randomly select the nation from selected range
+    if (side == 1) {
         console.log("blufor boi");
         deck += json.Blufor[getRNG(21)];
     }
@@ -25,13 +37,23 @@ function randomDeckGen(json, is_side_restricted = 0){
         
     console.log(deck);
 
+    // add the random deck spec after the nation
     deck += ' ' + json.DeckSpec[getRNG(6)];
 
     return deck;
 }
 
 $(function(){
+    /**
+     * Jquery stuff
+     * 
+     * Mainly used for button interaction
+     */
+    
+    // Initialize deck
     let deck = "";
+
+    // different button yield different number of random deck
     $("#gen_button1").click(function(){
         $.getJSON("etc/data/majsoul_result.json", function(json){
             deck = randomDeckGen(json);
@@ -66,6 +88,8 @@ $(function(){
         });
     });
 
+
+    // Dice rolling button
     $("#roll_dice_button").click(function(){
         num = getRNG(6) + 1;
         document.getElementById("output").innerHTML = num;
