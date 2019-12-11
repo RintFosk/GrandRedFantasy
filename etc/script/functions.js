558a1f39109682d9b4b152d838058bcbc7335761
Nicholas Yi Sheng Zeng <yszen2@student.monash.edu>
2019-12-11 11:00:52 +0800

    changed the getRNG function

--- etc/script/functions.js
+++ etc/script/functions.js
@@ -1,100 +1,109 @@
+const side = {
+	ALL_SIDE : 0,
+	BLUFOR   : 1,
+	REDFOR   : 2
+};
+
 function getRNG(max) {
-    /**
-     * Generate a random integer from 0 to (max-1)
+	/**
+     * Generate a random integer from 1 to max
      */
-    return Math.floor(Math.random() * (max));
+	return Math.floor(Math.random() * max) + 1;
 }
 
-function randomDeckGen(json, is_side_restricted = 0){
-    /**
+function randomDeckGen(json, side_restriction = side.ALL_SIDE) {
+	/**
      * Generate a random deck combination, faction side restriction is false by default
      */
 
-    let deck = '';
-    // Need to define what side the deck will be 
-    let side;
+	let deck = '';
+	// Need to define what side the deck will be
+	let side;
 
-    // if the side restriction is false, randomly decide the deck's faction within full range
-    if (is_side_restricted == 0){
-        side = getRNG(2);
-    }
-    // if the side restriction is on, force the side to be the parameter defined, 1 = blue, 2 = red
-    else {
-        side = is_side_restricted - 1;
-    }
-        
-    console.log(side)
+	// if the side restriction is false, randomly decide the deck's faction within full range
+	if (side_restriction == side.ALL_SIDE) {
+		side = getRNG(2);
+	}
+	else {
+		// if the side restriction is on, force the side to be the parameter defined, 1 = blue, 2 = red
+		side = side_restriction;
+	}
 
-    // randomly select the nation from selected range
-    if (side == 1) {
-        console.log("blufor boi");
-        deck += json.Blufor[getRNG(21)];
-    }
-    else {
-        console.log("redfor boi");
-        deck += json.Redfor[getRNG(13)];
-    }
-        
-    console.log(deck);
+	console.log(side);
 
-    // add the random deck spec after the nation
-    deck += ' ' + json.DeckSpec[getRNG(6)];
+	// randomly select the nation from selected range
+	switch (side) {
+		case side.BLUFOR:
+			console.log('blufor boi');
+			deck += json.Blufor[getRNG(21) - 1];
 
-    return deck;
+		case side.REDFOR:
+			console.log('redfor boi');
+			deck += json.Redfor[getRNG(13) - 1];
+	}
+
+	console.log(deck);
+
+	// add the random deck spec after the nation
+	deck += ' ' + json.DeckSpec[getRNG(6) - 1];
+
+	return deck;
 }
 
-$(function(){
-    /**
+$(function() {
+	/**
      * Jquery stuff
      * 
      * Mainly used for button interaction
      */
-    
-    // Initialize deck
-    let deck = "";
 
-    // different button yield different number of random deck
-    $("#gen_button1").click(function(){
-        $.getJSON("etc/data/majsoul_result.json", function(json){
-            deck = randomDeckGen(json);
-            document.getElementById("output").innerHTML = deck;
-        });
-    });
-    
+	// Initialize deck
+	let deck = '';
 
-    $("#gen_button2").click(function(){
-        $.getJSON("etc/data/majsoul_result.json", function(json){
-            deck = randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json);
-            document.getElementById("output").innerHTML = deck;
-        });
-    });
+	// different button yield different number of random deck
+	$('#gen_button1').click(function() {
+		$.getJSON('etc/data/majsoul_result.json', function(json) {
+			deck = randomDeckGen(json);
+			document.getElementById('output').innerHTML = deck;
+		});
+	});
 
-    
-    $("#gen_button3").click(function(){
-        $.getJSON("etc/data/majsoul_result.json", function(json){
-            deck = randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json) + 
-            "<br>" + randomDeckGen(json);
-            document.getElementById("output").innerHTML = deck;
-        });
-    });
+	$('#gen_button2').click(function() {
+		$.getJSON('etc/data/majsoul_result.json', function(json) {
+			deck = randomDeckGen(json) + '<br>' + randomDeckGen(json) + '<br>' + randomDeckGen(json);
+			document.getElementById('output').innerHTML = deck;
+		});
+	});
 
+	$('#gen_button3').click(function() {
+		$.getJSON('etc/data/majsoul_result.json', function(json) {
+			deck =
+				randomDeckGen(json) +
+				'<br>' +
+				randomDeckGen(json) +
+				'<br>' +
+				randomDeckGen(json) +
+				'<br>' +
+				randomDeckGen(json) +
+				'<br>' +
+				randomDeckGen(json) +
+				'<br>' +
+				randomDeckGen(json) +
+				'<br>' +
+				randomDeckGen(json) +
+				'<br>' +
+				randomDeckGen(json) +
+				'<br>' +
+				randomDeckGen(json) +
+				'<br>' +
+				randomDeckGen(json);
+			document.getElementById('output').innerHTML = deck;
+		});
+	});
 
-    // Dice rolling button
-    $("#roll_dice_button").click(function(){
-        num = getRNG(6) + 1;
-        document.getElementById("output").innerHTML = num;
-    });
-
-    
-
+	// Dice rolling button
+	$('#roll_dice_button').click(function() {
+		num = getRNG(6);
+		document.getElementById('output').innerHTML = num;
+	});
 });
