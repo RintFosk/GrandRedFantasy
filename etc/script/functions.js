3e9555a3307eef49594e7bb3a2eb1d06b59e9b6a
Rintfosk <27263945+RintFosk@users.noreply.github.com>
2019-12-14 01:31:18 +0800

    update on the deck conf generation mechanism

--- etc/script/functions.js
+++ etc/script/functions.js
@@ -1,55 +1,91 @@
 const sideENUM = {
-	ALL_SIDE: 0,
-	BLUFOR: 1,
-	REDFOR: 2
+	BLUFOR: 0,
+	REDFOR: 1,
+	ALL_SIDE: 2
+};
+
+const eraENUM = {
+	ERA_A: 0, // 1990
+	ERA_B: 1, // 1985
+	ERA_C: 2, // 1980
+	ALL_ERA: 3
 };
 
 function getRNG(max) {
 	/**
 	 * Generate a random integer from 0 to max - 1
 	 */
-	return Math.floor(Math.random() * max) + 1;
+	return Math.floor(Math.random() * max);
 }
 
-function randomDeckGen(json, side_restriction = sideENUM.ALL_SIDE) {
+function deckConfGen(json, side_restriction = sideENUM.ALL_SIDE, era_restriction = eraENUM.ALL_ERA) {
 	/**
-	 * Generate a random deck combination, faction side restriction is false by default
+	 * Generate a random deck configuration combination, faction side restriction is false by default
+	 * Contains League/Nation, Specialization and the deck era
 	 */
 
-	let deck = '';
+	// initialize the three conf components
+	let faction = "";
+	let spec = "";
+	let era = -1;
+
 	// Need to define what side the deck will be
 	let side;
 
+	// if the ERA restriction is false, randomly decide the deck's era within full range
+	if (era_restriction == eraENUM.ALL_ERA) {
+		era = getRNG(3);
+	} else {
+		// if the side restriction is on, force the side to be the parameter defined
+		era = era_restriction;
+	}
+
 	// if the side restriction is false, randomly decide the deck's faction within full range
 	if (side_restriction == sideENUM.ALL_SIDE) {
-		side = getRNG(2) + 1;
+		side = getRNG(2);
 	} else {
-		// if the side restriction is on, force the side to be the parameter defined, 1 = blue, 2 = red
+		// if the side restriction is on, force the side to be the parameter defined
 		side = side_restriction;
 	}
 
-	console.log(side);
-
-	// randomly select the nation from selected range
+	// randomly select the faction from selected range
 	switch (side) {
 		case sideENUM.BLUFOR:
-			console.log('blufor boi');
-			deck += json.Blufor[getRNG(21)];
+			faction = json.Blufor[getRNG(21)];
 			break;
 
 		case sideENUM.REDFOR:
-			console.log('redfor boi');
-			deck += json.Redfor[getRNG(13)];
+			faction = json.Redfor[getRNG(13)];
 			break;
 	}
 
-	console.log(deck);
-
 	// add the random deck spec after the nation
-	deck += ' ' + json.DeckSpec[getRNG(6)];
+	spec = json.DeckSpec[getRNG(6)];
 
-	return deck;
+	return [faction, spec, era];
 }
+
+
+function deckConfWrap(confArray) {
+	output = confArray[0] + ' ' + confArray[1];
+
+	switch (confArray[2]) {
+		case eraENUM.ERA_A:
+			output += ' 1990';
+			break;
+		case eraENUM.ERA_B:
+			output += ' 1985';
+			break;
+		case eraENUM.ERA_C:
+			output += ' 1980';
+			break;
+	}
+
+	output += "<br>";
+
+	return output;
+}
+
 
 $(function () {
 	/**
@@ -95,47 +131,30 @@ $(function () {
 
 	// different button yield different number of random deck
 	$('#gen_button1').click(function () {
-		$.getJSON('etc/data/league_nations.json', function (json) {
-			deck = randomDeckGen(json, side_restriction);
+		$.getJSON('etc/data/faction_and_deckSpec.json', function (json) {
+			deck = deckConfWrap(deckConfGen(json, side_restriction));
 			document.getElementById('output').innerHTML = deck;
+			deck = ""
 		});
 	});
 
 	$('#gen_button2').click(function () {
-		$.getJSON('etc/data/league_nations.json', function (json) {
-			deck =
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction);
+		$.getJSON('etc/data/faction_and_deckSpec.json', function (json) {
+			for (i = 0; i < 3; i++) {
+				deck += deckConfWrap(deckConfGen(json, side_restriction));
+			}
 			document.getElementById('output').innerHTML = deck;
+			deck = ""
 		});
 	});
 
 	$('#gen_button3').click(function () {
-		$.getJSON('etc/data/league_nations.json', function (json) {
-			deck =
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction) +
-				'<br>' +
-				randomDeckGen(json, side_restriction);
+		$.getJSON('etc/data/faction_and_deckSpec.json', function (json) {
+			for (i = 0; i < 10; i++) {
+				deck += deckConfWrap(deckConfGen(json, side_restriction));
+			}
 			document.getElementById('output').innerHTML = deck;
+			deck = ""
 		});
 	});
 
